1ab0725c0df6d2a9427b5f8a2c5088aa10667a39
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-09-26 09:55:52 +0300

    start with js

---
+++ lessons/lesson1.js
@@ -0,0 +1,18 @@
+/* 1.Hello World */
+
+console.log("Hello World!");
+
+/* Varriables */
+
+var firstName = "Nataliia"
+let lastName = "Petrychuk"
+console.log(firstName)
+console.log(lastName)
+
+var age, dateofBirth, sex
+age = "5";
+dateofBirth = "19/03/1982";
+sex = "Female";
+console.log(age)
+age = "6";
+console.log(age)
