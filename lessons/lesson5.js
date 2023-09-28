f4669713a3a3f2dbc3beb8225f1037d1c2e237f8
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-09-28 05:48:48 +0300

    update lesson5.js

--- lessons/lesson5.js
+++ lessons/lesson5.js
@@ -1,3 +1,16 @@
 //Logical "AND"
 
-console.log(true && true)
+console.log(true && true) // all valurs have to be True for expression to be True
+
+//Logical "OR"
+console.log(true || false) //any value should be True for the expression to be True
+
+var ageIsMoreThanEighteen  = true
+var isUSCitizen = true
+var elegibilityForDriversLicense = ageIsMoreThanEighteen && isUSCitizen
+
+console.log('This customer is eligible for DL:' + elegibilityForDriversLicense)
+
+//Logical operator "NOT"
+console.log(!true)
+console.log(6 !== 10)
