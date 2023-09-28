b0e7fae1df81a2c747aa0ee04350381fb298f2be
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-09-28 07:43:03 +0300

    Create lesson6 condition

---
+++ lessons/lesson6.js
@@ -0,0 +1,29 @@
+//conditional statement
+
+//if(condition) {
+    //execute some code here
+//} else {
+    //execute some code here
+//}
+
+//if hour between 6 and 12 print "Good morning"
+//if hour between 12 and 18 print "Good afternoon"
+//otherwise: Good evening 
+
+// var hour = 100;
+
+// if (hour >= 6 && hour < 12) {
+//   console.log("Good morning");
+// } else if (hour >= 12 && hour < 18) {
+//   console.log("Good afternoon");
+// } else {
+//   console.log("Good evening");
+// }
+var ageIsMoreThanEighteen = false;
+var isUSCitizen = true;
+
+if(ageIsMoreThanEighteen && isUSCitizen){
+    console.log('Customer is elegible for DL')
+} else {
+    console.log('Customer is NOT elegible for DL')
+}
