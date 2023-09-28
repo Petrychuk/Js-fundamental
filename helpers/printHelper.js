4804147a1fe98844ec9b8061cc55ff9c55aae135
NATALIIA PETRYCHUK <shadowpn@gmail.com>
2023-09-28 09:05:48 +0300

    classes in Js

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,3 +1,18 @@
 export function printAge(age) {
-    console.log(age)
+  console.log(age);
 }
+
+class CustomerDetails {
+  printFirstName(firstName) {
+    console.log(firstName);
+  }
+  /**
+   * this method will print the last name
+   * @param {string} lastName 
+   */
+  printLastName(lastName) {
+    console.log(lastName);
+  }
+}
+
+export const customerDetails = new CustomerDetails();
---
+++ lessons/lesson9.js
@@ -0,0 +1,9 @@
+// Импортируем класс CustomerDetails из модуля "../helpers/printHelper.js"
+import { customerDetails } from "../helpers/printHelper.js";
+
+// Теперь, после импорта, можно создать его экземпляр
+
+
+// Вызываем метод printFirstName у экземпляра класса
+customerDetails.printFirstName("Steve");
+customerDetails.printLastName('Smith')
